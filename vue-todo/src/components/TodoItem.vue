20dfe79eac756bdbd1a07c4f8ba0a76767b23311
Zeha Irawan <zehairawan@gmail.com>
2023-05-11 16:10:18 +0800

    init

---
+++ vue-todo/src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+  export default {
+    props:{
+      title: String
+    },
+    name:'TodoItem',
+  }
+</script>
+
+
+<template>
+  <main>
+    <span>{{ title }}</span>
+  </main>
+</template>
---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import TodoItem from './TodoItem.vue';
+</script>
+
+
+<template>
+  <main>
+  <h1>What is your goals today?</h1>
+  <TodoItem title="Buy Eggs"/>
+  </main>
+</template>
